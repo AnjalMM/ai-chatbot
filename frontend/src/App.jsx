dc279655b60ba9ec27c0e5d16403db786d4b20a2
anjal <mmanjal120@gmail.com>
2025-01-04 15:46:04 +0530

    to setup all routes for pages register verify home in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,22 @@
 /* eslint-disable no-unused-vars */
 import React from 'react'
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Home from './pages/Home'
+import Register from './pages/Register'
+import { Verify } from './pages/Verify'
 
 export default function App() {
   return (
-    <div className='text-red-600'>App</div>
+    <div>
+     <BrowserRouter>
+     <Routes>
+
+      <Route path='/' element={<Home/>}/>
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/verify' element={<Verify/>}/>
+
+     </Routes>
+     </BrowserRouter>
+    </div>
   )
 }
