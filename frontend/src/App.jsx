f82f7ce5790eca86ae7c15729f63546ae56f5a7c
anjal <mmanjal120@gmail.com>
2025-01-08 07:17:02 +0530

    tosetup the process of the verify section for otp recevice for users and auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,9 @@
 import React from 'react'
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Home from './pages/Home'
-import Register from './pages/Register'
 import { Verify } from './pages/Verify'
+import Login from './pages/Login'
+
 
 export default function App() {
   return (
@@ -12,7 +13,7 @@ export default function App() {
      <Routes>
 
       <Route path='/' element={<Home/>}/>
-      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
       <Route path='/verify' element={<Verify/>}/>
 
      </Routes>
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,34 +1,35 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React from 'react'
+import { useState } from 'react'
 
-export const Verify = () => {
-
-  const [otp,setotp]=useState("")
+export default function Login() {
+  const [email,setEmail]=useState("")
 
   const submitHandler =(e)=>{
     e.preventDefault()
-    console.log(otp);
+    console.log(email);
     
   }
   return (
     <div>
-      <div className="flex justify-center items-center h-screen">
+
+<div className="flex justify-center items-center h-screen">
       <form
         className="bg-white p-6 rounded shadow-md w-full
      md:w-[500px]"
      onSubmit={submitHandler}
       
       >
-        <h2 className="text-2xl mb-4">otp verify</h2>
+        <h2 className="text-2xl mb-4">Login</h2>
         <div className="mb-4">
           <label className="block text-gray-700 mb-2" htmlFor="email">
-            otp:
+            Email:
           </label>
           <input
-            type="otp"
-            id="otp"
-            value={otp}
-            onChange={e=>{setotp(e.target.value)}}
+            type="email"
+            id="email"
+            value={email}
+            onChange={e=>{setEmail(e.target.value)}}
             
             className="border p-2 w-full rounded outline-none focus:ring-2 focus:ring-blue-500"
             required
