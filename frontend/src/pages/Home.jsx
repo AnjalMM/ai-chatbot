10c956d4122a6710a6e62a5c8488cb0a3ee6496a
anjal <mmanjal120@gmail.com>
2025-01-11 03:02:08 +0530

    to setup the header section in homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,8 @@
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react'
 import Sidebar from '../components/Sidebar'
+import { GiHamburgerMenu } from "react-icons/gi";
+import Header from '../components/Header';
 
 export default function Home() {
 
@@ -12,6 +14,17 @@ export default function Home() {
   return (
     <div className='flex h-screen bg-gray-500 text-white'>
       <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
+
+     <div className='flex flex-1 flex-col'>
+      <button onClick={toggleSidebar} className='md:hidden p-4 bg-gray-800 text-2xl'>
+         <GiHamburgerMenu/>
+      </button>
+      <div className='flex-1 p-6 mb-20 md:mb-0'>
+       <Header/>
+      </div>
+
+     </div>
+
     </div>
   )
 }
